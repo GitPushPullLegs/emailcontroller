29c41e2bbd047200ebf879bb90d5b6503c36a99b
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-02-09 11:04:16 -0800

    Added fields to EmailContents

--- emailcontroller/emailcontents.py
+++ emailcontroller/emailcontents.py
@@ -1,16 +1,21 @@
 class EmailContents:
     """Provides the contents of an email in an easy-to-read format."""
-    def __init__(self, delivery_time, from_addr, subject, body):
+    def __init__(self, delivery_time, from_addr, to_addr, cc_addr, subject, plain_body, html_body):
         self.delivery_time = delivery_time
         self.from_addr = from_addr
+        self.to_addr = to_addr
+        self.cc_addr = cc_addr
         self.subject = subject
-        self.body = body
+        self.plain_body = plain_body
+        self.html_body = html_body
 
     def __str__(self):
         return f"""
-Delivered: {self.delivery_time}
-From:      {self.from_addr}
-Subject:   {self.subject}
+Delivered:  {self.delivery_time.strftime("%b %d, %Y at %I:%M:%S %p")}
+From:       {self.from_addr}
+To:         {self.to_addr}
+CC:         {self.cc_addr}
+Subject:    {self.subject}
         
-Body:
-{self.body}"""
+Plain Body: {'' if self.plain_body == '' else 'Available'}
+HTML Body:  {'' if self.html_body == '' else 'Available'}"""
